70bdb081cee7a3abfe67339a44e4124bbe4efe30
Ksenia <kseniaklokova@gmail.com>
2024-10-21 21:37:14 +0300

    решения семинара 1 и виселица

--- hangman.py
+++ hangman.py
@@ -43,9 +43,28 @@ HANGMAN_PICS =  ['''
        ===''']
 
 
+def print_introduction():
+   print("Добро пожаловать в Виселицу! Ты не можешь ошибиться больше 7 раз! Вводи по одной букве и смотри результаты.")
+   print("Для выхода из игры введи одно из слов: {}".format(", ".join(EXIT_WORDS)))
+   
+   
+def select_word(file_path="hangman_words.txt"):
+   with open(file_path, "r", encoding="utf-8") as f:
+      lines = f.readlines()
+   return lines
+
 # TODO
-            
+def main():
+   # Приветствие
+   # Выбрать слово
+   # Получаем инпут от пользователя (+ защита от дурака)
+   # Проверка буквы в слове (+ счетчик)
+   # Вывод промежуточных результатов с картиночкой
+   # Продолжаем играть?
+   print_introduction()
+   lines = select_word()
+   print(lines)
 
 if __name__ == "__main__":
-    main()
+   main()
 
